fb81bf9018f1324977632fd0c4eed212d44764d1
Mooncrown35 <mooncrown@outlook.com>
2026-09-23 16:43:18 +0300

    Update SettingsUIScreen.kt

--- app/src/main/java/com/lagradost/cloudstream3/ui/settings/SettingsUIScreen.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/settings/SettingsUIScreen.kt
@@ -148,7 +148,20 @@ object SettingsUIScreen : SearchableSettings {
                         }),
                 )
             ),
-
+	  Preference.PreferenceItem.ListPreference(
+                        preference = settings.ui.appFont,
+                        icon = painterResource(R.drawable.ic_baseline_text_fields_24),
+                        title = stringResource(R.string.app_font_settings),
+                        entries = stringArrayResource(R.array.app_font_values).zip(
+                            stringArrayResource(R.array.app_font_names)
+                        ).toMap().toPersistentMap(),
+                        onValueChanged = { newValue ->
+                            settings.ui.appFont.set(newValue)
+                            safe {
+                                activity?.recreate()
+                            }
+                            return@ListPreference false
+                        }),
             Preference.PreferenceGroup(
                 title = stringResource(R.string.pref_category_ui_features),
                 preferenceItems = persistentListOf(
